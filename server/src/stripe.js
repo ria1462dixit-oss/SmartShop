f698db2c53cdbaa71d268dc5d72b7717392160eb
ria1462dixit-oss <ria1462.dixit@gmail.com>
2026-04-10 03:08:40 +0530

    Add files via upload

---
+++ server/src/stripe.js
@@ -0,0 +1,43 @@
+import Stripe from 'stripe'
+
+function getStripe(secretKey) {
+  if (!secretKey) {
+    throw new Error('Set STRIPE_SECRET_KEY in server/.env to enable payments.')
+  }
+
+  return new Stripe(secretKey, {
+    apiVersion: '2026-02-25.clover',
+  })
+}
+
+export async function createCheckoutSession({ env, items, successUrl, cancelUrl, customerEmail }) {
+  const stripe = getStripe(env.STRIPE_SECRET_KEY)
+
+  const lineItems = (items || []).map((item) => ({
+    price_data: {
+      currency: 'inr',
+      product_data: {
+        name: item.name,
+        description: item.title || '',
+        images: item.image ? [item.image] : [],
+      },
+      unit_amount: Math.round(Number(item.amount || 0) * 100),
+    },
+    quantity: item.quantity || 1,
+  }))
+
+  if (!lineItems.length) {
+    throw new Error('No line items provided for payment.')
+  }
+
+  const session = await stripe.checkout.sessions.create({
+    mode: 'payment',
+    line_items: lineItems,
+    success_url: successUrl,
+    cancel_url: cancelUrl,
+    customer_email: customerEmail || undefined,
+    allow_promotion_codes: true,
+  })
+
+  return { id: session.id, url: session.url }
+}
